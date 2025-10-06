6efdd4d663c36762fffbb1bf3918a3f5c9c0029b
Trémus <19359682+Tremus@users.noreply.github.com>
2025-10-06 18:39:18 +1000

    First draft building structure to handle subdir watching

--- main.c
+++ main.c
@@ -1,3 +1,8 @@
+#define XHL_ALLOC_IMPL
+
+#define XARR_REALLOC xrealloc
+#define XARR_FREE    xfree
+
 #include <errno.h>
 #include <fcntl.h>
 #include <inttypes.h>
@@ -8,6 +13,9 @@
 #include <sys/time.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <xhl/alloc.h>
+#include <xhl/array.h>
+#include <xhl/debug.h>
 
 #define NUM_EVENT_SLOTS 1
 #define NUM_EVENT_FDS   1
@@ -65,6 +73,69 @@ char* flagstring(int flags)
     return ret;
 }
 
+// NOTE: kqueue and kevent don't watch items in subfolders folders, which means we have to walk through all the
+// subdirectories and set up watch handlers ourselves.
+// When we fetect new folders are created, we need to start watching them too, and when folders are
+// deleted, we need to stop watching them and their subdirectories.
+// When files are moved to the Trash on macOS, they may look like they're "renamed"
+
+struct WatchFolder
+{
+    int   fd;
+    char* path;
+};
+
+// https://man.freebsd.org/cgi/man.cgi?kqueue
+struct Context
+{
+    struct kevent*      events;  // xarray
+    struct WatchFolder* folders; // xarray
+};
+
+void ctx_init(struct Context* ctx)
+{
+    xarr_setcap(ctx->events, 64);
+    xarr_setlen(ctx->events, 0);
+    xarr_setcap(ctx->folders, 64);
+    xarr_setlen(ctx->folders, 0);
+};
+
+void ctx_deinit(struct Context* ctx)
+{
+    xarr_free(ctx->events);
+    xarr_free(ctx->folders);
+};
+
+// TODO: add items to context array
+void add_folder(struct Context* ctx, char* path)
+{
+    struct kevent event;
+    size_t        pathlen = strlen(path);
+
+    struct WatchFolder wf;
+    wf.path = xcalloc(1, pathlen + 1);
+
+    strcpy(wf.path, path);
+
+    wf.fd = open(path, O_EVTONLY);
+    if (wf.fd <= 0)
+    {
+        fprintf(stderr, "The file %s could not be opened for monitoring.  Error was %s.\n", path, strerror(errno));
+        exit(-1);
+    }
+
+    unsigned short flags = EV_ADD | EV_CLEAR;
+    unsigned int fflags  = NOTE_DELETE | NOTE_WRITE | NOTE_EXTEND | NOTE_ATTRIB | NOTE_LINK | NOTE_RENAME | NOTE_REVOKE;
+    EV_SET(&event, wf.fd, EVFILT_VNODE, flags, fflags, 0, path);
+
+    xarr_push(ctx->events, event);
+    xarr_push(ctx->folders, wf);
+
+    size_t nevents  = xarr_len(ctx->events);
+    size_t nfolders = xarr_len(ctx->folders);
+    xassert(nevents == nfolders);
+}
+
 int main()
 {
     int             kq                               = 0;
@@ -74,6 +145,8 @@ int main()
     struct timespec timeout                          = {0};
     unsigned int    vnode_events                     = 0;
     char*           path                             = WATCH_DIR;
+
+    xalloc_init();
 
     kq       = kqueue();
     event_fd = open(path, O_EVTONLY);
@@ -126,5 +199,8 @@ int main()
     }
     printf("exit loop");
     close(event_fd);
+
+    xalloc_shutdown();
+
     return 0;
 }
